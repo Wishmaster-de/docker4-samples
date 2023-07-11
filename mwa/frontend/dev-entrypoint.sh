26cce10e87bfb7c2223d82ce7041bb0647d8661f
Bernd Oeggl <bernd@webman.at>
2023-07-11 14:27:16 +0200

    fix: entrypoint with replace

--- mwa/frontend/dev-entrypoint.sh
+++ mwa/frontend/dev-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # zur sicherheit alle npm Pakete installieren
+cd /src/vue
 npm i
 
-API_BASE=${API_BASE:-https://diaryapi.dockerbuch.info}
+API_BASE=${API_BASE:-https://api.dockerbuch.info}
 
-sed -i "s|^const apiBase = .*$|const apiBase = '$API_BASE'|" \
+sed -i "s|baseUrl:.*$|baseUrl: '$API_BASE',|" \
 	  /src/vue/config/index.js
 
 exec "$@"
